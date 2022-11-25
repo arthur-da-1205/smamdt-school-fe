3f09e5877af5718df5bb84ab38972108aacc2429
Arthur Darwanto <75304260+arthur-da-1205@users.noreply.github.com>
2022-11-25 10:49:55 +0700

    setup prettier eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import './App.css';
+import React, { useState } from 'react';
+import reactLogo from './assets/react.svg';
 
 const App: React.FC = () => {
   const [count, setCount] = useState(0);
@@ -25,9 +25,7 @@ const App: React.FC = () => {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </div>
   );
 };
